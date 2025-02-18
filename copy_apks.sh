f23d92a3613feb3ffb07311d966715802bfc5dfe
zoranl <zrn.lkc@gmail.com>
2025-02-18 17:37:40 +0100

    updated files, added copy script

---
+++ copy_apks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+
+cp /d/androidWorkspace/MyMath/app/build/outputs/apk/debug/com.zoranl.mymath-v1\(0.9b\)-debug.apk /d/mygit/MySharedApk/
+cp /d/androidWorkspace/BTapp/app/build/outputs/apk/debug/com.zoranl.btapp-v2\(0.9b\)-debug.apk  /d/mygit/MySharedApk/
+cp /d/androidWorkspace/WiFIApp/app/build/outputs/apk/debug/com.zoranl.wifiapp-v1\(0.8\)-debug.apk /d/mygit/MySharedApk/
+cp /d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1\(0.0\)-debug.apk /d/mygit/MySharedApk/
