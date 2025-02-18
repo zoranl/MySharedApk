782e4cb76b0dbd694b9d9b358b6f0da36dd45266
zoranl <zrn.lkc@gmail.com>
2025-02-18 20:54:39 +0100

    updating changed files

--- git_push_changed.sh
+++ git_push_changed.sh
@@ -5,9 +5,20 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo 
 echo !!!!current branch:$current_branch
 echo 
+
+if [ $current_branch != master ]; then
+	echo !!!!not on master branch!!!!
+	exit
+fi
+
 git status
 
 changed_files=$(git diff --name-only --no-color)
+
+if [ $changed_files == "" ]; then
+	echo !!!!no files changed!!!!
+	exit
+fi
 
 echo
 echo !!!!changed files:$changed_files
