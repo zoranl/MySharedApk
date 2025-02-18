0337f23d7be244223853d56a16f4c56e512c04f1
zoranl <zrn.lkc@gmail.com>
2025-02-18 20:41:16 +0100

    added script for pushing files

---
+++ git_push_changed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo 
+echo !!!!current branch:$current_branch
+echo 
+git status
+
+changed_files=$(git diff --name-only --no-color)
+
+echo
+echo !!!!changed files:$changed_files
+echo
+
+git add $changed_files
+
+git commit -m "updating changed files"
+git push -u origin master
