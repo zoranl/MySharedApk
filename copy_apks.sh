3c1da24e3f5684a20051b3ab06c9f430656d639e
zoranl <zrn.lkc@gmail.com>
2025-03-05 21:54:15 +0100

    updating changed files

--- copy_apks.sh
+++ copy_apks.sh
@@ -4,7 +4,7 @@
 #cp /d/androidWorkspace/MyMath/app/build/outputs/apk/debug/com.zoranl.mymath-v1\(1.0b\)-debug.apk  /d/mygit/MySharedApk/
 #cp /d/androidWorkspace/BTapp/app/build/outputs/apk/debug/com.zoranl.btapp-v2\(0.9b\)-debug.apk    /d/mygit/MySharedApk/
 #cp /d/androidWorkspace/WiFIApp/app/build/outputs/apk/debug/com.zoranl.wifiapp-v1\(0.8\)-debug.apk /d/mygit/MySharedApk/
-#cp /d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1\(0.0\)-debug.apk   /d/mygit/MySharedApk/
+#cp /d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1\(0.1b\)-debug.apk   /d/mygit/MySharedApk/
 
 
 
@@ -14,7 +14,7 @@ FILE_0=("/d/androidWorkspace/MyApp/app/build/outputs/apk/debug/com.zoranl.myapp-
 FILE_1=("/d/androidWorkspace/MyMath/app/build/outputs/apk/debug/com.zoranl.mymath-v1(1.0b)-debug.apk" "/d/mygit/MySharedApk/com.zoranl.mymath-v1(1.0b)-debug.apk")
 FILE_2=("/d/androidWorkspace/BTapp/app/build/outputs/apk/debug/com.zoranl.btapp-v2(0.9b)-debug.apk" "/d/mygit/MySharedApk/com.zoranl.btapp-v2(0.9b)-debug.apk")
 FILE_3=("/d/androidWorkspace/WiFIApp/app/build/outputs/apk/debug/com.zoranl.wifiapp-v1(0.8)-debug.apk" "/d/mygit/MySharedApk/com.zoranl.wifiapp-v1(0.8)-debug.apk")
-FILE_4=("/d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1(0.0)-debug.apk" "/d/mygit/MySharedApk/com.zoranl.nfcapp-v1(0.0)-debug.apk")
+FILE_4=("/d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1(0.1b)-debug.apk" "/d/mygit/MySharedApk/com.zoranl.nfcapp-v1(0.1b)-debug.apk")
 APK_ARRAY=(
   FILE_0[@]
   FILE_1[@]
@@ -82,7 +82,7 @@ FILE_DESTINATION=/d/mygit/MySharedApk/com.zoranl.wifiapp-v1\(0.8\)-debug.apk
 check_files
 
 FILE_SOURCE=/d/androidWorkspace/NfcApp/app/build/outputs/apk/debug/com.zoranl.nfcapp-v1\(0.0\)-debug.apk
-FILE_DESTINATION=/d/mygit/MySharedApk/com.zoranl.nfcapp-v1\(0.0\)-debug.apk
+FILE_DESTINATION=/d/mygit/MySharedApk/com.zoranl.nfcapp-v1\(0.1b\)-debug.apk
 
 check_files
 }
